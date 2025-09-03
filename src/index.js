8186eb2106d5362d0ddb59922eebdb2d81b72d06
htm-ellie <html.elitt@gmail.com>
2025-09-03 11:16:31 -0400

    added search engine functionality in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("Hello");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-box-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
